7e592760f129b37a106edbe042bb50b464e73e46
yundaji <97157839+yundaji@users.noreply.github.com>
2026-06-23 15:54:51 +0800

    Reorder channels and add Telegram API credentials

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 BOT_TOKEN = "8970432708:AAHbdyqeDCM57p9ljmkzyYa0DBaIiyTyqR4"
 
 CHANNELS = [
-    "@sanji255",
-    "@douzi25"
+    "@douzi25",
+    "@sanji255"
 ]
 
 DAILY_COUNT = 5
 POOL_LIMIT = 300
+
+# Telegram API（只用于抓历史）
+API_ID = 37199356
+API_HASH = "d9156458a8520ad1d227f14c43ee19e7"
