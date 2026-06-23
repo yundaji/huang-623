47ef84e7320f8c8c704d177a2c7eb15c496d1ff7
yundaji <97157839+yundaji@users.noreply.github.com>
2026-06-23 16:08:08 +0800

    Refactor message sending functions using Telethon

--- tg.py
+++ tg.py
@@ -1,26 +1,23 @@
-import requests
-from config import BOT_TOKEN
+from telethon.sync import TelegramClient
+from config import API_ID, API_HASH
+
+client = TelegramClient("session", API_ID, API_HASH)
 
 
-# 单条消息
-def send_single(chat_id, message_id, from_chat):
+def send_album(chat_id, message_ids):
 
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/copyMessage"
+    # 🔥 关键：grouped=True 才是相册
+    with client:
 
-    requests.post(url, data={
-        "chat_id": chat_id,
-        "from_chat_id": from_chat,
-        "message_id": message_id
-    })
+        client.send_file(
+            chat_id,
+            message_ids,
+            grouped=True
+        )
 
 
-# 相册（重点：不拆图）
-def send_album(chat_id, message_ids, from_chat):
+def send_single(chat_id, message_id):
 
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/copyMessages"
+    with client:
 
-    requests.post(url, data={
-        "chat_id": chat_id,
-        "from_chat_id": from_chat,
-        "message_ids": message_ids
-    })
+        client.send_message(chat_id, message_id)
