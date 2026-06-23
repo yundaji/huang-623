b05a7c845e75848f0b1743234d3169e0bf2cebf5
yundaji <97157839+yundaji@users.noreply.github.com>
2026-06-23 15:58:08 +0800

    Refactor message sending functions in tg.py

--- tg.py
+++ tg.py
@@ -1,14 +1,26 @@
 import requests
 from config import BOT_TOKEN
 
-def send_message(chat_id, message_id, from_chat):
+
+# 单条消息
+def send_single(chat_id, message_id, from_chat):
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/copyMessage"
 
-    data = {
+    requests.post(url, data={
         "chat_id": chat_id,
         "from_chat_id": from_chat,
         "message_id": message_id
-    }
+    })
 
-    requests.post(url, data=data)
+
+# 相册（重点：不拆图）
+def send_album(chat_id, message_ids, from_chat):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/copyMessages"
+
+    requests.post(url, data={
+        "chat_id": chat_id,
+        "from_chat_id": from_chat,
+        "message_ids": message_ids
+    })
